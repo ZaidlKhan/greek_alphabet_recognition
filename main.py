fc806243f4b9db72339907c635dea12bc48bd655
ZaidlKhan <110517508+ZaidlKhan@users.noreply.github.com>
2023-12-21 12:08:52 -0800

    added js file

--- main.py
+++ main.py
@@ -1,18 +1,47 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+from bidict import bidict
+from random import choice
+import numpy as np
+
+
+ENCODER = bidict({
+    'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6,'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15, 
+    'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26
+})
 
 app = Flask(__name__)
+app.secret_key = "quiz"
 
 @app.route('/')
 def index():
+    session.clear()
     return render_template("index.html")
 
 @app.route("/add-data", methods=['GET'])
 def add_data_get():
-    return render_template("addData.html")
+    message = session.get("message", "")
+    labels = np.load("data/labels.npy")
+    count = {k : 0 for k in ENCODER.keys}
+    for label in labels:
+        count[label] += 1
+    count = sorted(count.items(), key=lambda x: x[1])
+    letter = count[0][0]
+    return render_template("addData.html", letter=letter, message = message)
 
 @app.route("/add-data", methods=['POST'])
 def add_data_post():
-    return render_template("addData.html")
+    label = request.form["letter"]
+    labels = np.array([label])
+    np.save("data/labels.npy", labels)
+    pixels = request.form["pixels"]
+    pixels = pixels.split(",")
+    session["message"] = "\"label \" added to the training dataset"
+    img = np.array(pixels).astype(float).reshape(1, 50, 50)
+    imgs = np.load("data/images.npy")
+    imgs = np.vstack([img, imgs])
+    np.save("data/images.npy", img)
+
+    return redirect(url_for("add_data_get"))
 
 @app.route("/practice", methods=['GET'])
 def practice_get():
@@ -24,4 +53,7 @@ def practice_post():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
     
